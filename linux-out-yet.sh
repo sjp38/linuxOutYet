503689ac7d95b2065e7c5ae63fd76159aa6fdb60
SeongJae Park <sj38.park@gmail.com>
2020-12-19 10:09:47 +0100

    linux-out-yet.sh: Support stable versions
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- linux-out-yet.sh
+++ linux-out-yet.sh
@@ -8,8 +8,19 @@ fi
 
 version=$1
 
-url="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tag/"
-url+="?h=$version"
+git_kernel_org_url="https://git.kernel.org/pub/scm/linux/kernel/git/"
+
+nr_numbers=$(echo $version | awk -F'.' '{print NF}')
+
+if [ $nr_numbers -eq 2 ]
+then
+	url="$git_kernel_org_url/torvalds/linux.git/tag/?h=$version"
+elif [ $nr_numbers -eq 3 ]
+then
+	url="$git_kernel_org_url/stable/linux.git/tag/?h=$version"
+else
+	echo "Unsupported version"
+fi
 
 if curl -sI "$url" | grep -qe "^HTTP/1.1 200 OK"
 then
